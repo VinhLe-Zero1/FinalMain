ab1aa8f56229cf995765ba4e7b0f8a78596e865d
ductuantruong <36566080+ductuantruong@users.noreply.github.com>
2019-05-13 17:36:38 +0700

    Add class NewInfos and class NewInfosController
    thêm lớp NewInfos và NewInfosController cho feature capnhatthongtin

---
+++ capnhapthongtin/capnhap/NewInfos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capnhap
+{
+    class NewInfos
+    {
+        private int personal_ID;
+        private string phone;
+        private string date_of_birth;
+        private string email;
+        private string hometown_addr;
+        private string house_addr;
+        public void addNewInfos(int personal_ID, string phone, string date_of_birth, string email, string hometown_addr, string house_addr)
+        {
+            this.personal_ID = personal_ID;
+            this.phone = phone;
+            this.date_of_birth = date_of_birth;
+            this.email = email;
+            this.hometown_addr = hometown_addr;
+            this.house_addr = house_addr;
+        }
+        public int getPersonal_ID() { return this.personal_ID; }
+        public string getPhone() { return this.phone; }
+        public string getDate_of_birth() { return this.date_of_birth; }
+        public string getEmail() { return this.email; }
+        public string getHometown_addr() { return this.hometown_addr; }
+        public string getHouse_addr() { return this.house_addr; }
+    }
+}
---
+++ capnhapthongtin/capnhap/NewInfosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace capnhap
+{
+    class NewInfosController
+    {
+        public DataSet GetDBCommand(string query)
+        {
+            DataSet data = new DataSet();
+            using (System.Data.SqlClient.SqlConnection connection = new SqlConnection("Data Source = DESKTOP - RHERCMS\SQLEXPRESS; Initial Catalog = PatientInfo; Integrated Security = True"))
+            {
+                connection.Open();
+                SqlDataAdapter adaper = new SqlDataAdapter(query, connection);
+                adaper.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
